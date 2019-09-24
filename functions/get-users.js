4e5b21e46f6a7c4dec9e5b136ecf37c8abf8a2cc
Bilal Haidar <bhaidar@gmail.com>
2019-09-24 12:38:50 +0300

    implement cloud function

--- functions/get-users.js
+++ functions/get-users.js
@@ -1,6 +1,29 @@
-exports.handler = function(event, context, callback) {
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify(event)
-	});
-}
+const axios = require('axios');
+
+const getUsers = (event, context, callback) => {
+	const { CLIENT_ID, CLIENT_SECRET } = process.env;
+	const API_URL = 'https://api.github.com/users';
+	const URL = `${API_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+	
+	// send user response
+	const send = body => {
+			callback(null, {
+				statusCode: 400,
+				body: JSON.stringify(body)
+			});
+	}
+
+	// perform API call
+	const get = () => {
+		axios.get(URL)
+			.then(res => send(res.data))
+			.catch(err => send(err));
+	}
+
+	// allow only GET requests
+	if (event.httpMethod === 'GET') {
+		get();
+	}
+};
+
+exports.handler = getUsers;
